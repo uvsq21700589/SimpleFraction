e71b96f9b3b56f14ab6cf8ae055c77475474fac5
uvsq21700589 <kawiilliam78@gmail.com>
2022-09-29 14:38:05 +0200

    Introduction de toString "Je suis une fraction."

--- Fraction.java
+++ Fraction.java
@@ -1,3 +1,11 @@
 public class Fraction {
-    
+    private String chaine = "Je suis une fraction.";
+    public Fraction(){
+
+    }
+
+    public String toString(){
+        return chaine;
+
+    }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        Fraction f = new Fraction();
+        System.out.println(f.toString());
     }
 }
