8fd0a7fba13b80a9724cca4e46609bf126e54c9a
uvsq21700589 <kawiilliam78@gmail.com>
2022-09-29 16:46:23 +0200

    Partie 2 ex 7

--- Fraction.java
+++ Fraction.java
@@ -1,11 +1,71 @@
 public class Fraction {
-    private String chaine = "Je suis une fraction.";
-    public Fraction(){
+
+    private int numerateur;
+    private int denominateur;
+    private static final int ZERO = 0;
+    private static final int UN = 1;
+
+    public Fraction(int numerateur, int denominateur) {
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
+
+    public Fraction(int numerateur) {
+        this.numerateur = numerateur;
+        this.denominateur = 1;
+    }
+
+    public Fraction() {
+        this.numerateur = 0;
+        this.denominateur = 1;
+    }
+
+    public String toString() {
+        String chaine = numerateur + "/" + denominateur;
+        return chaine;
 
     }
 
-    public String toString(){
-        return chaine;
+    public int getNumerateur() {
+        return numerateur;
+    }
 
+    public int getDenominateur() {
+        return denominateur;
+    }
+
+    public double doubleValue() {
+        double d = (double) numerateur / denominateur;
+        return d;
+    }
+
+    public Fraction add(Fraction g) {
+        int tmp1 = this.getDenominateur();
+        int tmp2 = g.getDenominateur();
+
+        Fraction r = new Fraction(this.getNumerateur() * tmp2 + g.getNumerateur() * tmp1, tmp1 * tmp2);
+
+        return r;
+    }
+
+    public boolean FracEquals(Fraction g) {
+        int tmp1 = g.Pgcd();
+        int tmp2 = this.Pgcd();
+        Fraction t = new Fraction(g.getNumerateur() / tmp1, g.getDenominateur() / tmp1);
+        Fraction u = new Fraction(this.getNumerateur() / tmp2, this.getDenominateur() / tmp2);
+        return t.toString().equals(u.toString());
+    }
+
+    public int Pgcd() {
+        int tmp1 = this.getNumerateur();
+        int tmp2 = this.getDenominateur();
+        while (tmp1 != tmp2) {
+            if (tmp1 > tmp2)
+                tmp1 = tmp1 - tmp2;
+            else
+                tmp2 = tmp2 - tmp1;
+
+        }
+        return tmp2;
     }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        Fraction f = new Fraction();
-        System.out.println(f.toString());
+        Fraction f = new Fraction(2, 4);
+        Fraction g = new Fraction(4, 8);
+
+        int t = f.getNumerateur() / f.getDenominateur();
+        //assert t == 5 / 4 : "La Fraction ne correspond pas.";
+
+        System.out.println(f.FracEquals(g));
+        
     }
 }
